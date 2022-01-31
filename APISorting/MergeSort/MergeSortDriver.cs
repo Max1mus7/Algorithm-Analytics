cbd7a25000c003e98123c14059442c0d3f1f585d
Max1mus7 <maxdweber7@gmail.com>
2022-01-31 13:11:42 -0700

    Added projects for MergeSort and QuickSort
    In addition, also confirmed that calling GetType() on a member of a list will return true when compared to an object of the same type.

---
+++ APISorting/MergeSort/MergeSortDriver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSortDriver
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
